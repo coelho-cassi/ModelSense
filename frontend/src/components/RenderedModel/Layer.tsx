1076cecad2aa00fd556edccf39de9cfc92dae6bb
TJ <coelhocassi@gmail.com>
2023-12-17 21:10:25 -0600

    Layer now grows to allow more neurons up to 207 before weird behaviors start

--- frontend/src/components/RenderedModel/Layer.tsx
+++ frontend/src/components/RenderedModel/Layer.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { RoundedBox } from "@react-three/drei";
 
-const Layer = ({ xPos, layerIndex, getColorForLayer }) => {
-  const layerHeight = 5; // Define the fixed height for each layer here
+const Layer = ({ xPos, nodeCount, layerIndex, getColorForLayer }) => {
+  const layerBaseHeight = 5; // Define the fixed height for each layer here
+  const maxNeuronsPerRow = 9;
+  const baseRows = 9;
+  const numRows = Math.ceil(nodeCount / maxNeuronsPerRow);
+  const additionalRows = Math.max(numRows - baseRows, 0);
+  const rowHeight = layerBaseHeight / baseRows;
+  const layerHeight = layerBaseHeight + additionalRows * rowHeight;
+  const maxHeight = rowHeight * 23;
+  const finalHeight = Math.min(layerHeight, maxHeight);
+
+  const yPosOffset = -(additionalRows * rowHeight * 0.5);
+
   return (
     <RoundedBox
-      args={[5, layerHeight, 0.2]}
-      position={[xPos, 0, 0]}
+      args={[5, finalHeight, 0.2]}
+      position={[xPos, yPosOffset, 0]}
       rotation={[0, Math.PI / 2, 0]}
     >
       <meshPhysicalMaterial
